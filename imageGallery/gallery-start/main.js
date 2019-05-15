c0e21cf5f6adec2f280ff16f8b5e577e99f340be
SmritiMdr <SmritiMdr@users.noreply.github.com>
2019-05-15 15:31:58 +0545

    Websites

---
+++ imageGallery/gallery-start/main.js
@@ -0,0 +1,37 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+for(var i = 1; i <= 5; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+  thumbBar.appendChild(newImage);
+  newImage.onclick = function(a) {
+    var imgs = a.target.getAttribute('src');
+    displayImages(imgs);
+  }
+}
+
+function displayImages(imgs) {
+  displayedImage.setAttribute('src', imgs);
+}
+
+/* Wiring up the Darken/Lighten button */
+
+btn.onclick = function() {
+  var colours = btn.getAttribute('class'); 
+  if(colours=== 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } 
+  else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+}
